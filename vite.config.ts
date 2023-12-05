83305f50262e8807bea6c6111bfb158004c5a576
ZhouYang <xinzhuxiansheng@126.com>
2023-12-06 02:01:54 +0800

    add

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  server: {
+    host: '127.0.0.1',
+    port: 8080,
+    proxy: {
+      '/api': 'http://react.yzhou.com:5000' // 此处一定要配置域名，不然无法访问
+    }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
+  plugins: [react()]
 })
